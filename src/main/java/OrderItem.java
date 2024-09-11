202c2d72edd434e9f2051240d4dee8e8dd49ddfb
Foo Tun Wei <tunwei0302@gmail.com>
2024-09-11 20:57:20 +0800

    updated orderItem class

--- src/main/java/OrderItem.java
+++ src/main/java/OrderItem.java
@@ -1,13 +1,14 @@
-public class OrderItem extends Item {
+public class OrderItem {
+    private Item item;
     private int orderQuantity;
 
     public OrderItem(String itemName, int itemQuantity, ItemGroups itemGroup, int orderQuantity) {
-        super(itemName, itemQuantity, itemGroup);
+        this.item = new Item(itemName, itemQuantity, itemGroup);
         this.orderQuantity = orderQuantity;
     }
 
     public OrderItem(String name, int quantity, ItemGroups groupName, double buyPrice, int maxInvLV, int minInvLV, int orderQuantity) {
-        super(name, quantity, groupName, buyPrice, maxInvLV, minInvLV);
+        this.item = new Item(name, quantity, groupName, buyPrice, maxInvLV, minInvLV);
         this.orderQuantity = orderQuantity;
     }
 
@@ -17,5 +18,13 @@ public class OrderItem extends Item {
 
     public void setOrderQuantity(int orderQuantity) {
         this.orderQuantity = orderQuantity;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
     }
 }
